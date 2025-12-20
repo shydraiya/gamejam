27c58996e372f15bda18afa2b803c2e92b5a4e8c
hyeok2044 <hyeok2044@gmail.com>
2025-12-21 05:02:48 +0900

    pushing

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -5,6 +5,7 @@ public class MainMenuController : MonoBehaviour
 {
     public string gameSceneName = "Game";
     public string storySceneName = "Story";
+    public string mainSceneName = "MainMenu";
 
     // 0 = TopView, 1 = FPS
     const string StartModeKey = "StartMode";
@@ -14,6 +15,12 @@ public class MainMenuController : MonoBehaviour
         PlayerPrefs.SetInt(StartModeKey, 0);
         PlayerPrefs.Save();
         SceneManager.LoadScene(gameSceneName);
+    }
+    public void MainMenu()
+    {
+        PlayerPrefs.SetInt(StartModeKey, 0);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(mainSceneName);
     }
 
     public void Story()
